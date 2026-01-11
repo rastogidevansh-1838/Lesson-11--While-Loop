e08ff506b7c7f9ee4079c9ebe56e16c5c0ae8edf
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-01-11 10:44:24 +0530

    my superbbb projecto

--- hw.py
+++ hw.py
@@ -1,8 +1,6 @@
-num_a = int(input("Enter number A: "))
-a = num_a
-num_b = int(input("Enter number B: "))
-b = num_b
-num_c = int(input("Enter number C: "))
-c = num_c
-total = c+a+b
-print(f"{total} is the total of your question")
+num = int(input("Enter a number: "))
+count = 0
+while num > 0:
+    count += 1
+    num //= 10
+print("Total digits:", count)
