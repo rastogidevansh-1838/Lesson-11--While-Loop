c0375c71be500ef8a1039e4996342e5cb16e9103
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-01-11 10:29:13 +0530

    MY projecttt

---
+++ hw.py
@@ -0,0 +1,8 @@
+num_a = int(input("Enter number A: "))
+a = num_a
+num_b = int(input("Enter number B: "))
+b = num_b
+num_c = int(input("Enter number C: "))
+c = num_c
+total = c+a+b
+print(f"{total} is the total of your question")
